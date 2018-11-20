5403262d5c60046f3e90f95c44cb1611755b6d68
peter <petecruckshank@gmail.com>
2018-11-19 20:59:56 -0500

    init basic api fetch data call to db.json

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-// const greeting = 'Hello World';
-// console.log(greeting);
+import {http} from './http';
 
-// const getData = async (url) => {
-//   const response = await fetch(url);
-//   const result = await response.json();
-//   console.log(result);
-// };
+// Get posts on DOM load 
+document.addEventListener('DOMContentLoaded', getPosts);
 
-// getData('https://jsonplaceholder.typicode.com/posts');
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
