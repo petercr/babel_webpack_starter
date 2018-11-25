479c401f0d6f49ead971ef3fe17761f8757389d0
Peter <petecruckshank@gmail.com>
2018-11-24 21:33:47 -0500

    added clearInput() & changeFormState() to the scope of the project This takes care of the cancel button functionality

--- src/ui.js
+++ src/ui.js
@@ -4,7 +4,7 @@ class UI {
     this.titleInput = document.querySelector('#title');
     this.bodyInput = document.querySelector('#body');
     this.idInput = document.querySelector('#id');
-    this.postInput = document.querySelector('.post-submit');
+    this.postSubmit = document.querySelector('.post-submit');
     this.forState = 'add';
 
   }
@@ -77,6 +77,44 @@ class UI {
     this.titleInput.value = data.title;
     this.bodyInput.value = data.body;
     this.idInput.value = data.id;
+
+    this.changeFormState('edit');
+  }
+
+  // Clear ID hidden value
+  clearIdInput() {
+    this.idInput = '';
+  }
+
+  // change the form state
+  changeFormState(type){
+    if(type === 'edit'){
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      // Create cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+
+      // Get parent 
+      const cardForm = document.querySelector('.card-form');
+      // Get parent to insert before
+      const formEnd = document.querySelector('.form-end');
+      // Insert cancel button
+      cardForm.insertBefore(button, formEnd);
+    } else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+      // Remove cancel btn if present
+      if(document.querySelector('.post-cancel')){
+        document.querySelector('.post-cancel').remove();
+      }
+      // Clear ID from hidden field
+      this.clearIdInput();
+      // Clear text
+      this.clearFields();
+    }
   }
 }
 
